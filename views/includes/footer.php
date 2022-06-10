2485062a3ea09f2fbf0de8230cc8a46484785248
Dayanna <equipodetrabajosdjr@gmail.com>
2022-06-10 11:41:57 -0500

    zona de herencia, creada

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -69,13 +69,8 @@
         <div class="container">
             <div class="copyright">
                 <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a href="#">Your Site Name</a>, All Right Reserved.
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a href="https://htmlcodex.com">HTML Codex</a>
-                        <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0" >
+                        &copy; <a href="#">Asesorías sitios web</a>, All Right Reserved.
                     </div>
                 </div>
             </div>
