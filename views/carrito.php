169d812a5efda7f4219617244dce43db01fa0cd5
santiago orozco <equipodetrabajosdjr@gmail.com>
2022-06-10 20:14:49 -0500

    correcion

--- views/carrito.php
+++ views/carrito.php
@@ -3,8 +3,6 @@
 <html lang="en">
 
 <head>
-
-
     <meta charset="utf-8">
     <title>Solartec - Renewable Energy Website Template</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
